4b4fd17822b1ee00ab79a94f9393dbf9a4615499
zehei <zeheiw@gmail.com>
2019-04-18 20:32:47 +0800

    Add files via upload

--- scripts/action.js
+++ scripts/action.js
@@ -20,30 +20,49 @@ function deleteCubes(gameWindow, freezedCubes, deletedCubes){
 	}
 }
 
-function getFullCubesRow(cubes){
-	var countCubesPerRow = new Array(GAME_WINDOW_HEIGHT).fill(0);
-	var fullCubesRow = new Array();
-	var fullCubesRowCoord = new Array();
+function checkFullCubesRow(cubes){
+	for (var i = GAME_WINDOW_HEIGHT; i >= 0; i--){
+		for (var j = cubes.length - 1; j >= 0; j--) {
+			getCoord(getPosition(cubes[j]))
+		}
+		getCoord()
+	}
+}
+
+function countNumberOfCubesPerRow(cubes){
+	var numberOfCubesPerRow = new Array(GAME_WINDOW_HEIGHT).fill(0);
+
 	for (var i = cubes.length - 1; i >= 0; i--) {
 		coordY = getCoord(getPosition(cubes[i])[1]);
-		if (coordY < 0 || coordY > GAME_WINDOW_HEIGHT){
-		}
-		else {
-			countCubesPerRow[coordY] += 1;
+		if (coordY >= 0 && coordY <= GAME_WINDOW_HEIGHT) { 
+			numberOfCubesPerRow[coordY] += 1; 
 		}
 	}
-	if (countCubesPerRow.indexOf(GAME_WINDOW_WIDTH) != -1) {
-		for (var i = countCubesPerRow.length - 1; i >= 0; i--) {
-			if (countCubesPerRow[i] == GAME_WINDOW_WIDTH) {
-				fullCubesRowCoord.push(i);
+	return numberOfCubesPerRow;
+}
+
+function getCoordOfFullRow(cubes){
+	var numberOfCubesPerRow = countNumberOfCubesPerRow(cubes);
+	var coordOfFullRow = new Array()
+	for (var i = numberOfCubesPerRow.length - 1; i >= 0; i--) {
+		if (numberOfCubesPerRow[i] == GAME_WINDOW_WIDTH) {	
+			coordOfFullRow.push(i);
+		}
+	}
+	return coordOfFullRow;
+}
+
+function getFullRowList(cubes){
+	coordOfFullRow = getCoordOfFullRow(cubes);
+	var fullRowList = new Array();
+	for (var i = coordOfFullRow.length - 1; i >= 0; i--) {
+		var fullRow = new Array();
+		for (var j = cubes.length - 1; j >= 0; j--) {
+			if (getCoord(getPosition(cubes[j])[1]) == coordOfFullRow[i]) {
+				fullRow.push(cubes[j]);
 			}
 		}
+		fullRowList.push(fullRow);
 	}
-	for (var i = cubes.length - 1; i >= 0; i--) {
-		coordY = getCoord(getPosition(cubes[i])[1]);
-		if (fullCubesRowCoord.indexOf(coordY) != -1) {
-			fullCubesRow.push(cubes[i]);
-		}
-	}
-	return fullCubesRow;
+	return fullRowList;
 }
